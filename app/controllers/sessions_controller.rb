59d36fc8fadabb33f7e6f8770150781bf2c81930
github username <github email address>
2020-05-12 19:07:50 -0700

    find, set, authenticate user log in user to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController 
     def new
-
+        @user = User.new
     end
 
     def create 
-
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to root_path
+        else
+            redirect_to login_path, alert: "User not found. Please try again."
+        end
     end
 end
