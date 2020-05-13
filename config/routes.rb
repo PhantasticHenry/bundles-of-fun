e11928095d2be2bcb914920cbef34986bd16bee8
github username <github email address>
2020-05-12 19:36:05 -0700

    add sign up form and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'signup', to: 'users#new'
+  
 
   root 'static_pages#home'
 end
