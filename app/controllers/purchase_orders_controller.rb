b2ce7bcc9bd000d2ecd9861bf93f6afd331af2be
github username <github email address>
2020-05-12 20:17:56 -0700

    add index, new, create actions & strong params

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,2 +1,24 @@
 class PurchaseOrdersController < ApplicationController
+    
+    def index 
+        @purchase_orders = PurchaseOrder.all
+    end
+    
+    def new 
+        @purchase_order = PurchaseOrder.new
+    end
+    
+    def create 
+        @purchase_order = PurchaseOrder.new(po_params)
+        if @purchase_order.save
+            redirect_to purchase_orders_path
+        else 
+            render :new
+        end
+    end
+
+    private 
+    def po_params
+        params.require(:purchase_order).permit(:po, :bin, :start_date, :completion_date)
+    end
 end
