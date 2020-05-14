10048278b67118b3399766c093c4d48ee9afe399
github username <github email address>
2020-05-14 14:06:03 -0700

    instantiate new product add strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
     def new 
-
+        @product = Product.new
     end
-
+    
     def create 
-
+        @product = Product.new(product_params)
     end
 
     def show 
@@ -21,6 +21,11 @@ class ProductsController < ApplicationController
     end
 
     def destroy
-        
+
+    end
+
+    private 
+    def product_params
+        params.require(:product).permit(:name, :color, :size, :sku, :category, :user_id, :purchase_order_id)
     end
 end
