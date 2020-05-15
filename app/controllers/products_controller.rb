0caed5a927c329229c784b542822f48b8e2b5a59
github username <github email address>
2020-05-15 15:44:17 -0700

    fix index action error, add table to index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
     before_action :set_product, only: [:show, :edit, :update, :destroy]
 
     def index 
-        @product = Product.all
+        @products = Product.all
     end
 
     def new 
