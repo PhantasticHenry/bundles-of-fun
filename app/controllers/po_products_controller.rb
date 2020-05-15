9afa18358cde11f218d63fbed0f7c742d1905b1d
github username <github email address>
2020-05-15 15:20:42 -0700

    add actions and strong params

--- app/controllers/po_products_controller.rb
+++ app/controllers/po_products_controller.rb
@@ -1,2 +1,50 @@
 class PoProductsController < ApplicationController
+       
+    def index 
+        @po_product = PoProduct.all
+    end
+
+    def new
+        if params[:purchase_order_id] && purchase_order = PurchaseOrder.find_by(id: params[:purchase_order_id])
+            @po_product = purchase_order.products.build
+        else
+            @po_product = PoProduct.new
+        end
+    end
+    
+    def create
+        @po_product = product.po_products.build(po_product_params)
+        if @po_product.save!
+            redirect_to po_product_path(@po_product)
+        else 
+            render :new
+        end
+    end
+
+    def show 
+        set_po_product
+    end
+
+    def edit 
+        
+    end
+
+    def update 
+
+    end
+
+    def destroy
+
+    end
+
+    private 
+    def product_params
+        params.require(:po_product).permit(:quantity, :user_id, :purchase_order_id)
+    end
+
+    def set_po_product
+        @po_product = PoProduct.find_by(id: params[:id])
+    end
+end
+
 end
