550fe27eae49b2f8177541dd04b50048fc7a1430
github username <github email address>
2020-05-15 16:01:19 -0700

    validates presence title case method for bin

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,6 +1,15 @@
 class PurchaseOrder < ApplicationRecord
     belongs_to :user
-    has_many :po_products, :dependent => :delete_all
-    has_many :products, through: :po_products, :dependent => :delete_all
+
+    has_many :po_products
+    has_many :products, through: :po_products
+    
+    validates :po, :bin, :start_date, :completion_date, presence: true
+
+    private 
+
+    def title_case_bin
+        self.bin = bin.uppercase
+    end
 
 end
