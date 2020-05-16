dfad730f9ac53e86d05d0e0ff9ab516adab8a2b3
github username <github email address>
2020-05-16 14:08:17 -0700

    add has_many association with products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :purchase_orders
+  has_many :products
 
   validates :name, :email, :password, presence: true
   validates :name, :email, uniqueness: true
