a3aca1e6d065b26afe7548e47bcd0c0325fdb71b
github username <github email address>
2020-05-16 01:05:01 -0700

    add actions strong params before actions

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,2 +1,43 @@
 class MaterialsController < ApplicationController
+    before_action :logged_in?, only: [:edit, :destroy]
+    before_action :set_material, only: [:show, :edit, :update, :destroy]
+
+    def index 
+        @materials = Material.all
+    end
+
+    def new 
+        @material = Material.new
+    end
+    
+    def create
+        @material = Material.new(material_params)
+        if @material.save && !material_params.empty?
+            redirect_to materials_path
+        else 
+            render :new
+        end
+    end
+
+    def show
+        
+    end
+
+    def edit 
+
+    end
+
+    def update
+    
+    end
+
+    private 
+    def material_params
+        params.require(:material).permit(:name, :color, :size, :sku, :category )
+    end
+
+    def set_material
+        @material = Material.find_by(id: params[:id])
+    end
+
 end
