6dcdfb684a2bcc61fd1551e3cc415935e35c51bc
github username <github email address>
2020-05-16 14:07:44 -0700

    add belongs_to association with user

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  belongs_to :user 
   has_many :po_products
   has_many :purchase_orders, through: :po_products
   validates :name, :color, :size, :sku, :category, presence: true
