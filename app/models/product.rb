f7ffcddcfdb6045ded3eb155d9478af45d07694a
github username <github email address>
2020-05-16 23:26:06 -0700

    add has_many and has_many through

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,11 @@ class Product < ApplicationRecord
   belongs_to :user 
   has_many :po_products
   has_many :purchase_orders, through: :po_products
+  has_many :product_materials
+  has_many :materials, through: :product_materials
+  
   validates :name, :color, :size, :sku, :category, presence: true
   validates :name, :sku, uniqueness: true
+
+  
 end
