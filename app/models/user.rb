d885ad2a09c6a9435705725ae79a7e04d6198657
github username <github email address>
2020-05-16 20:03:26 -0700

    add has_many materials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :purchase_orders
   has_many :products
+  has_many :materials
 
   validates :name, :email, :password, presence: true
   validates :name, :email, uniqueness: true
