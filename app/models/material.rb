094be94479d0ec13d323e88898cf7566e5648caf
github username <github email address>
2020-05-16 23:28:50 -0700

    add has_many associations

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,5 +1,11 @@
 class Material < ApplicationRecord
+    
     belongs_to :user
+
+    has_many :product_materials
+    has_many :products, through: :product_materials
+
     validates :name, :color, :size, :sku, :category, presence: true
     validates :name, :sku, uniqueness: true
+
 end
