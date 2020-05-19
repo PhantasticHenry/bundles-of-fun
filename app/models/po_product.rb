33de8db0f77bd01ea0c32d0a6872c748a0e0dc3e
github username <github email address>
2020-05-18 20:55:50 -0700

    add multicam_color scope

--- app/models/po_product.rb
+++ app/models/po_product.rb
@@ -2,4 +2,6 @@ class PoProduct < ApplicationRecord
   belongs_to :purchase_order, optional: true
   belongs_to :product, optional: true
   validates :quantity, presence: true
+
+  scope :multicam_color, -> {where(color: "multicam")}
 end
