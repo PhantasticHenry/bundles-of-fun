8413e69f4187d1f2b5d74aa33ddc9aaf8feab836
github username <github email address>
2020-05-18 21:48:14 -0700

    remove multicam_color action

--- app/controllers/po_products_controller.rb
+++ app/controllers/po_products_controller.rb
@@ -11,10 +11,6 @@ class PoProductsController < ApplicationController
         end
     end
 
-    def multicam_color
-        @po_products = PoProduct.all.multicam_color
-    end
-
     def new
         @po_product = PoProduct.new(purchase_order_id: params[:purchase_order_id])
     end
