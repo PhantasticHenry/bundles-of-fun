552ee00a63c8f295f7393dc755a3dff3b311031d
github username <github email address>
2020-05-20 13:39:08 -0700

    add uid:string to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_033631) do
+ActiveRecord::Schema.define(version: 2020_05_20_203808) do
 
   create_table "materials", force: :cascade do |t|
     t.string "name"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_033631) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
   add_foreign_key "materials", "users"
